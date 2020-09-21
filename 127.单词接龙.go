288a11571780bd0127dd9db06944d4b2d18b3c5c
liuguili <gongzili456@gmail.com>
2020-09-21 14:11:57 +0800

    ----

--- 127.单词接龙.go
+++ 127.单词接龙.go
@@ -1,7 +1,6 @@
 package leetcode
 
 import (
-	"fmt"
 	"strings"
 )
 
@@ -11,10 +10,9 @@ import (
  * [127] 单词接龙
  */
 
-// TODO 单词接龙
 // @lc code=start
 func ladderLength(beginWord string, endWord string, wordList []string) int {
-	// 预处理，找出单词的通用状态
+	// 预处理，找出单词的通用状态，构建图结构
 	dic := map[string][]string{}
 	for _, w := range wordList {
 		for i := 0; i < len(w); i++ {
@@ -23,15 +21,37 @@ func ladderLength(beginWord string, endWord string, wordList []string) int {
 		}
 	}
 
-	vis := map[string]bool
-	q := []string{beginWord}
-	depth := 0
-
-	for len(q) > 0 {
-		depth++
-
-
+	type record struct {
+		word  string
+		depth int
 	}
+
+	vis := map[string]bool{}
+	q := []record{record{beginWord, 1}}
+
+	// BFS 图遍历
+	for len(q) > 0 {
+		head := q[0]
+		q = q[1:]
+
+		if head.word == endWord {
+			return head.depth
+		}
+
+		vis[head.word] = true
+
+		for i := 0; i < len(head.word); i++ {
+			k := strings.Join(append([]string(nil), head.word[:i], "*", head.word[i+1:]), "")
+			if v, ok := dic[k]; ok {
+				for _, s := range v {
+					if !vis[s] {
+						q = append(q, record{word: s, depth: head.depth + 1})
+					}
+				}
+			}
+		}
+	}
+
 	return 0
 }
 
